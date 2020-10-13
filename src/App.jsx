4dfd3240aa62a58c2f839b5a55c0549c65e24e9e
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-10-13 00:42:35 -0500

    change props for students rendering

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ class App extends React.Component {
     if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
-          placeholder="Add Students..." 
+          placeholder="Add Student..." 
           currList={this.state.students}
           addFunction={this.addStudent}
           title="Student Roster"
